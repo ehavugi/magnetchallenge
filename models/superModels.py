658da1b0eb2b96f5608cf609ee480c423352a6ad
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2024-04-28 15:10:09 -0700

    Added correlation of error with global std from population of models

--- models/superModels.py
+++ models/superModels.py
@@ -11,6 +11,7 @@ materials=["Volumetric_Loss_Material A.csv",
         "Volumetric_Loss_Material C.csv",
         "Volumetric_Loss_Material D.csv",
         "Volumetric_Loss_Material E.csv"]
+schools = []
 for material in materials:
     data={}
     for school in os.listdir(path):  # list of subdirectories and files
@@ -21,12 +22,14 @@ for material in materials:
             matpath= f"{path}/{school}/Result/{material}"
             if os.path.isfile(matpath):
                 datai=  pd.read_csv(f"{path}/{school}/Result/{material}",header=None)
-                
+                schools.append(school)
                 # print("Datai", datai.describe())
                 data[school]=np.abs(datai.values.reshape(-1))
                 # print("data", data.describe())
     data=pd.DataFrame.from_dict(data)
     print(data.columns)
+    # Select university to use in the modeling of std correlation with percentile errors
+    # uncomment selection to disable selection 
     # data=data[[ 'Paderborn', 'Sydney','Tsinghua', 'TUDelft', 'XJTU']]
     # data=data[[ 'ASU', 'Tribhuvan']]
 
@@ -44,9 +47,6 @@ for material in materials:
     correct="../finaltest/EvaluationKit/EvaluationKit/Measured_"+material
     # print(correct)
     datac = pd.read_csv(correct,header=None)
-    # data[]
-    # print(datac.values.shape,data['ASU'].values.shape)
-
     print("school, median error, 95 th percentile,99 percentile,99.9 percentile")
 
     for each in ['ASU', 'KU Leuven', 'Mondragon', 'NTUT', 'Paderborn', 'PoliTO', 'Sydney', 'Tribhuvan', 'Tsinghua', 'TUDelft', 'UTK', 'XJTU']:
@@ -58,38 +58,41 @@ for material in materials:
     print("gmean", np.percentile(data['error'],50),np.percentile(data['error'],95),np.percentile(data['error'],99),np.percentile(data['error'],99.9))
 
 
-    plt.plot(data['std'],data['error'],"*")
-    plt.ylabel("Error(%)")
-    plt.xlabel("standard deviation")
+    # plt.plot(data['std'],data['error'],"*")
+    # plt.ylabel("Error(%)")
+    # plt.xlabel("standard deviation")
     print(np.corrcoef(data['std'],data['error']))
-    n = len(data)
-    datacopy=data.copy(True)
-    dataccopy=datac.copy(True)
-    percentileErrors = []
-    percentileRejectionRatio = []
-    for RejectionRatio in range(1,99):
-        reject=np.percentile(datacopy['std'].values,RejectionRatio)
-        datac= dataccopy[datacopy['std'].values>reject]
+   
+    factor = "gmean"
+    factors = [x  for x in list(data.columns) if x in schools]
+    factors.append("gmean")
+    for factor in factors:
+        n = len(data)
 
-        data=datacopy[datacopy['std'].values>reject]
-        # print("Bayesian Error(given a condition detectable)")
-        data['error']=np.abs(data["gmean"].values.reshape(-1)-datac.values.reshape(-1))/datac.values.reshape(-1)
-        # print("gmean after filtering: ", np.percentile(data['error'],50),np.percentile(data['error'],95),np.percentile(data['error'],99),np.percentile(data['error'],99.9))
-        # for each in ['ASU', 'KU Leuven', 'Mondragon', 'NTUT', 'Paderborn', 'PoliTO', 'Sydney', 'Tribhuvan', 'Tsinghua', 'TUDelft', 'UTK', 'XJTU']:
-        #     if each in data.columns:
-        #         data['error']=np.abs(data[each].values.reshape(-1)-datac.values.reshape(-1))/datac.values.reshape(-1)
-        #         # print(each,np.percentile(data['error'],50), np.percentile(data['error'],95),np.percentile(data['error'],99),np.percentile(data['error'],99.9))
-        percentileErrors.append(np.percentile(data['error'],95))
-        percentileRejectionRatio.append(RejectionRatio)
-        # print(len(data)/n*100, "%","remaining")
-    # print(data.index)
+        datacopy=data.copy(True)
+        dataccopy=datac.copy(True)
+        percentileErrors = []
+        percentileRejectionRatio = []
+        rejectThreshold=[]
+        for RejectionRatio in range(1,99):  # percentage of data select by std across models
+            reject=np.percentile(data['std'].values,RejectionRatio)
+            # Select a subset of the data 
+            dataccopy= datac[data['std'].values>reject]
+            datacopy=data[data['std'].values>reject]
 
-    plt.figure()
-    plt.plot(percentileRejectionRatio,percentileErrors)
-    # plt.plot(percentileRejectionRatio, percentileErrors)
+            datacopy['error']=np.abs(datacopy[factor].values.reshape(-1)-dataccopy.values.reshape(-1))/dataccopy.values.reshape(-1)
+            # print(len(datacopy))
+            percentileErrors.append(np.percentile(datacopy['error'],95))
+            percentileRejectionRatio.append(RejectionRatio)
+            rejectThreshold.append(reject)
+            # print(len(data)/n*100, "%","remaining")
 
-    plt.ylabel("Error")
-    plt.xlabel("Percentile rejected by std metric")
-    plt.savefig(material+".png")
-    # plt.show()
+        plt.figure()
+        plt.plot(rejectThreshold,percentileErrors)
+        plt.title(f"95th Percentile Errors(%) vs rejection threshold\n normalized std error(%) {material}")
+
+        plt.ylabel("95th percentile Error(%)")
+        plt.xlabel("coefficient of variation(%)")
+        plt.savefig(factor+"_"+material+".png")
+# plt.show()
 
